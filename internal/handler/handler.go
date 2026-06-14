b89f4156345cbadbd9acb4be5ae987e32669f97f
sailtyip <saltycroser@gmail.com>
2026-06-14 22:06:18 +0530

    race conditon for bid placing and end/started bid confusion fixed
    race condition for bid placing fixed for multiple users usign mutex
    
    also a way to distinguish between started and end auction state also
    implemented

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,62 +2,87 @@
 package handler
 
 import (
-	"auction/internal/models"
 	"context"
 	"encoding/json"
 	"net/http"
 	"strconv"
+	"sync"
 	"time"
+
+	"auction/internal/models"
 
 	"github.com/redis/go-redis/v9"
 )
 
-func Getbid(redisconn *redis.Client) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
+func Getbid(redisconn *redis.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var ResponseBid models.GetBid
-		bid := redisconn.Get(context.Background(), "Bid")
-		ResponseBid.Bid = bid.Val()
-		w.Header().Set("Content-Type","application/json")
+		exist := redisconn.Exists(context.Background(), "Bid").Val()
+		endcheck := redisconn.Get(context.Background(), "Start").Val()
+		if exist == 0 {
+			if endcheck == "true" { // end state
+				http.Error(w, "Auction ended", http.StatusNotFound)
+				return
+			}
+			http.Error(w, "Auction not started/ set", http.StatusServiceUnavailable)
+			return
+		}
+		bid := redisconn.Get(context.Background(), "Bid").Val()
+		ResponseBid.Bid = bid
+		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(&ResponseBid)
 	}
 }
 
+// end check 3 States
+// bid exist and nto empty -> auction going on
+// bid no exist -> can be auction end/ auction start
+// thats why we need start
 
-func Bid(redisconn *redis.Client) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
+
+func Bid(redisconn *redis.Client, m *sync.Mutex) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var UserBody models.UserBid
 		json.NewDecoder(r.Body).Decode(&UserBody)
-		highestbid,err := strconv.Atoi(redisconn.Get(context.Background(),"Bid").Val())
-		if err!=nil{
-			http.Error(w,"Auction not set",http.StatusServiceUnavailable)
+		userbid, err := strconv.Atoi(UserBody.UserBid)
+		if err != nil {
+			http.Error(w, "error conversion", http.StatusBadRequest)
 			return
 		}
-		userbid,err := strconv.Atoi(UserBody.UserBid)
-		if err != nil{
-			http.Error(w,"error conversion",http.StatusBadRequest)
+		// mutexes locks this part for protecting race condition
+		m.Lock()         // this is mutex format lock then defer then the code(critical section) to protect
+		defer m.Unlock() // this solves teh issue of the 2 comments below written as thsi runs as soon as below retuns
+		highestbid, err := strconv.Atoi(redisconn.Get(context.Background(), "Bid").Val())
+		if err != nil {
+			endcheck := redisconn.Get(context.Background(), "Start").Val()
+			if endcheck == "true" {
+				http.Error(w, "Auction ended ", http.StatusNotFound)
+				return // here i need ot set a m.Unlock to unlock this part if bid is not set
+			}
+			http.Error(w, "Auction not set", http.StatusServiceUnavailable)
 			return
+
 		}
-		if userbid > highestbid{
+		if userbid > highestbid {
 			expire := redisconn.ExpireTime(context.Background(), "Bid").Val()
-			redisconn.Set(context.Background(), "Bid", UserBody.UserBid,expire)
-			redisconn.Set(context.Background(), "IP", r.RemoteAddr,expire)
+			redisconn.Set(context.Background(), "Bid", UserBody.UserBid, expire)
+			redisconn.Set(context.Background(), "IP", r.RemoteAddr, expire)
 			w.WriteHeader(http.StatusAccepted)
-		}else{
-			w.WriteHeader(http.StatusConflict)	
+		} else {
+			w.WriteHeader(http.StatusConflict)
 		}
+		// m.Unlock() -> putting this alone here will not take care of the return if bid is not set yet
 	}
 }
 
-
-func Setbid(redisconn *redis.Client) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
+func Setbid(redisconn *redis.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		var setredis models.Setredis
 
 		json.NewDecoder(r.Body).Decode(&setredis)
-		expiry := setredis.Expiry * int(time.Second)	
-		redisconn.Set(context.Background(), "Bid",setredis.Bid,time.Duration(expiry))
-		redisconn.Set(context.Background(), "IP",setredis.IP,time.Duration(expiry))
-
-	
+		expiry := setredis.Expiry * int(time.Second)
+		redisconn.Set(context.Background(), "Bid", setredis.Bid, time.Duration(expiry))
+		redisconn.Set(context.Background(), "IP", setredis.IP, time.Duration(expiry))
+		redisconn.Set(context.Background(), "Start", "true", time.Duration(0))
 	}
 }
